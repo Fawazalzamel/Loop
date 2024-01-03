e97bc4b8127fcbedba9e18b6bcbaf51d778d46f6
Fawaz J M M A Alzamel <83331@kfh.com>
2024-01-03 14:04:34 +0300

    Loops Task

---
+++ Loop/src/WeekdaysPrinter.java
@@ -0,0 +1,12 @@
+public class WeekdaysPrinter {
+    public static void main(String[] args) throws Exception {
+        String[] DaysOfTheWeek={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+   for(int i=0; i<DaysOfTheWeek.length;i++ ){
+if (i<5) {
+    
+    System.out.println(DaysOfTheWeek[i]);
+
+}else break;
+   }
+    }
+}
